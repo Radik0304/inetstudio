3fb491c59caa1f627d15c9d54ad07d4ad269deb8
tarzemanovrr <tarzemanovrr@63pokupki.ru>
2023-10-04 22:48:28 +0400

    in work

---
+++ src/components/Users.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-container>
+        <v-col
+          cols="12"
+      >
+        <v-card
+            max-width="450"
+            class="mx-auto"
+        >
+          <v-list three-line>
+            <template v-for="(item, index) in users">
+              <v-subheader
+                  v-if="item.header"
+                  :key="item.header"
+                  v-text="item.header"
+              ></v-subheader>
+
+              <v-divider
+                  v-else-if="item.divider"
+                  :key="index"
+                  :inset="item.inset"
+              ></v-divider>
+
+              <v-list-item
+                  v-else
+                  :key="item.title"
+              >
+                <v-list-item-avatar>
+                  <v-img :src="item.avatar"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title v-html="item.title"></v-list-item-title>
+                  <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                    <v-list-item-score v-html="item.score"></v-list-item-score>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-card>
+
+      </v-col>
+    </v-container>
+</template>
+
+<script>
+// import store from '../store/store';
+export default {
+    name: 'Users',
+}
+</script>
---
+++ src/components/UsersFilter.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <v-col cols="12" md="4">
+            <v-select v-model="variant" :items="country" clearable label="Filter by country"></v-select>
+            <v-select v-model="variant" :items="score" clearable label="Filter by score"></v-select>
+        </v-col>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'UsersFilters',
+    data: () => ({
+        variant_country: 'default',
+        variant_score: 'default'
+    }),
+}
+</script>
